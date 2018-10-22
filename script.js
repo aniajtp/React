a8f8fc5c616d5f6be4c48614e9405d90a55b3859
Anna Półtorak <aniajtp@gmail.com>
2018-10-22 17:47:40 +0200

    w React.createElement było ('img', {}, movie.a) jest ('img', (src:movie.a}). Zmieniono połozenie zmiennej var element

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var movies = [
         title: 'Harry Potter',
         desc: 'Film o czarodzieju',
         info: 'Gatunek: familijny, fantasy, przygodowy',
-        a: 'https://www.filmonpaper.com/site/media/2011/07/HarryPotterAndTheSorcerersStone_onesheet_USA_DrewStruzan-1-500x730.jpg'
+        a: 'https://ssl-gfx.filmweb.pl/po/05/71/30571/7253557.2.jpg'
     },
     {
         id: 2,
         title: 'Król Lew',
         desc: 'Film o królu sawanny',
         info: 'Gatunek: animacja, familijny',
-        a: 'https://ssl-gfx.filmweb.pl/po/68/78/6878/7389475.3.jpg'
+        a: 'https://ssl-gfx.filmweb.pl/po/68/78/6878/6999844.2.jpg'
     },
     {
         id: 3,
@@ -25,7 +25,7 @@ var movies = [
         title: 'Lista Schindlera',
         desc: 'Historia przedsiebiorcy, który podczas II wojny swiatowej ratował Żydów',
         info: 'Gatunek: dramat',
-        a: 'https://ssl-gfx.filmweb.pl/po/08/62/862/7517878.2.jpg'
+        a: 'https://ssl-gfx.filmweb.pl/po/12/11/1211/7075581.2.jpg'
     },
     {
         id: 5,
@@ -35,19 +35,21 @@ var movies = [
         a: 'https://ssl-gfx.filmweb.pl/po/43/78/694378/7632114.2.jpg',
 }
 ];
-var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
 
 var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc),
         React.createElement('p', {}, movie.info),
-        React.createElement('img', {}, movie.a)
-        )
-    });
+        React.createElement('img', {src:movie.a},)
+    )
+});
+
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements),
+);
+
 ReactDOM.render(element, document.getElementById('app'));
 
